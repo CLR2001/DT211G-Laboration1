18268d60eae5021536d3afcdf557e06f692fe34a
CLR2001 <luro2501@student.miun.se>
2026-01-21 12:04:14 +0100

    HTML för undersidan bilder klar.

--- src/js/pages/bilder-page.js
+++ src/js/pages/bilder-page.js
@@ -1,6 +1,24 @@
+import { getOptimizedImage } from "../image-optimizer.js";
+import raccoonUrl from "../../assets/images/raccoon.jpg?w=400;600;800;1200&format=avif;webp;jpg&as=metadata";
+import birdUrl from "../../assets/images/bird.jpg?w=400;600;800;1200&format=avif;webp;jpg&as=metadata";
+import polarUrl from "../../assets/images/polar.jpg?w=400;600;800;1200&format=avif;webp;jpg&as=metadata";
+import foxUrl from "../../assets/images/fox.jpg?w=400;600;800;1200&format=avif;webp;jpg&as=metadata";
+import sheepUrl from "../../assets/images/sheep.jpg?w=400;600;800;1200&format=avif;webp;jpg&as=metadata";
+import koalaUrl from "../../assets/images/koala.jpg?w=400;600;800;1200&format=avif;webp;jpg&as=metadata";
+
 export const bilderPage = `
-  <h1>Bilder</h1>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam suscipit quidem eaque, saepe corrupti temporibus facere magni ratione pariatur voluptatibus harum quis doloremque excepturi porro culpa maiores beatae aliquid? Impedit?
-  </p>
+  <section class="main-content-bilder">
+    <h1>Bilder</h1>
+    <p>
+      Nedan är ett par bilder tagna från <a href="https://unsplash.com/">unsplash.com</a>. De är optimerade med hjälp av vite-plugin-image-optimzer. Dessutom har olika storlekar för varje bild skapats med hjälp av vite-imagetools. Dessa bilder har en bredd på 400px, 600px, 800px och 1200px.
+    </p>
+    <div class="images-container">
+      ${getOptimizedImage(raccoonUrl, {alt: "Tvättbjörn som går på gräs", layout: "grid"})}
+      ${getOptimizedImage(birdUrl, {alt: "Färglad fågel sitter på en gren", layout: "grid"})}
+      ${getOptimizedImage(polarUrl, {alt: "Isbjörn sitter med ena tassen upp", layout: "grid"})}
+      ${getOptimizedImage(foxUrl, {alt: "Räv i ett snöigt landskap", layout: "grid"})}
+      ${getOptimizedImage(sheepUrl, {alt: "Får som tiitar i i kameran", layout: "grid"})}
+      ${getOptimizedImage(koalaUrl, {alt: "Koala som sover i ett träd", layout: "grid"})}
+    </div>
+  </section>
 `
