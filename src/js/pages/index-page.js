530ac4a320a1f25a82bf13367adfee0e94271783
CLR2001 <luro2501@student.miun.se>
2026-01-21 15:51:34 +0100

    HTML för startsida klar

--- src/js/pages/index-page.js
+++ src/js/pages/index-page.js
@@ -1,6 +1,29 @@
 export const indexPage = `
-  <h1>Index</h1>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam suscipit quidem eaque, saepe corrupti temporibus facere magni ratione pariatur voluptatibus harum quis doloremque excepturi porro culpa maiores beatae aliquid? Impedit?
-  </p>
+  <section class="main-content-index">
+    <h1>Startsida</h1>
+    <section>
+      <h2>Projektbeskrivning</h2>
+      <p>
+        Denna laboration syftar till att automatisera större delar av arbetet inom webbutveckling. Genom att implementera automatisering sparar man både tid och undviker mänskliga fel som kan uppkomma när man hanterar större projekt med flertalet filer.
+      </p>
+    </section>
+    <section>
+      <h2>Verktyg</h2>
+      <ul>
+        <li><b>Vite</b>: Ett modernt frontend byggverktyg.</li>
+        <li><b>Git & GitHub</b>: Versionshanteringsverktyg för lokala och publicerade repon.</li>
+        <li><b>Netlify</b>: Modern plattform för hosting av webbplatser. Kan länkas med exempelvis GitHub för automatiserad publicering av nya versioner.</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Automatisering</h2>
+      <ul>
+        <li>Publicering</li>
+        <li>Minifiering</li>
+        <li>Konkatenering & transpilering</li>
+        <li>Generering av bilder till olika format och storlekar</li>
+        <li>Optimering av bilder</li>
+      </ul>
+    </section>
+  </section>
 `;
